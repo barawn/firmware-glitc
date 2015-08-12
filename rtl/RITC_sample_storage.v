e55b0fddce293d4e8e7dfa0ca27073ea64664f31
Cloudreaver <ryanhupe@gmail.com>
2015-08-12 14:24:27 -0400

    RITC sample storage update
    Fixed errors in RITC sample storage. Now reads out 8 3-bit samples at a
    time.

--- rtl/RITC_sample_storage.v
+++ rtl/RITC_sample_storage.v
@@ -21,32 +21,24 @@ module RITC_sample_storage(
 		output done_o
     );
 
-	wire [31:0] user_block0_data;
-	wire [31:0] user_block1_data;
-	wire [31:0] user_block2_data;
-	wire [31:0] user_data[3:0];
-	// Low 32 bits of R0 data.
-	assign user_data[0] = user_block0_data;
-	// High word of R0 and R1 data. R0 data is in the low 16 bits.
-	assign user_data[1] = user_block1_data;
-	// Low 32 bits of R1 data.
-	assign user_data[2] = user_block2_data;
-	// High word of R0 and R1 data, but reversed so R1 data is in the low 16 bits.
-	// Helps with reading out individual RITCs.
-	assign user_data[3] = {user_block1_data[15:0],user_block1_data[31:16]};
-	assign user_dat_o = user_data[user_addr_i[10:9]];
+	wire [31:0] user_block_data[7:0];
+	assign user_dat_o = user_block_data[user_addr_i[10:8]];
 	
 	reg [8:0] write_addr = {9{1'b0}};
 	wire [9:0] write_addr_plus_one = write_addr + 1;
-	reg [8:0] read_addr = {9{1'b0}};
+	reg [9:0] read_addr = {10{1'b0}};
 	reg write_enable = 0;
 	reg write_was_enabled = 0;
 	reg addr0_sync = 0;
 	wire [7:0] byte_write_enable = {8{write_enable}};
-	wire [71:0] R0_input = {{24{1'b0}},C,B,A};
-	wire [71:0] R1_input = {{24{1'b0}},F,E,D};
-	wire [71:0] R0_output;
-	wire [71:0] R1_output;
+	
+	wire [35:0] A_output[1:0];
+	wire [35:0] B_output[1:0];
+	wire [35:0] C_output[1:0];
+	wire [35:0] D_output[1:0];
+	wire [35:0] E_output[1:0];
+	wire [35:0] F_output[1:0];
+	
 	reg read_enable=0;
 	reg write_done=0;
 	reg [2:0] write_done_user_clk = {2{1'b0}};
@@ -85,23 +77,77 @@ module RITC_sample_storage(
 				read_enable <= 1;
 		else
 			read_enable <= 0;
-		if (!write_done_user_clk[1]) read_addr <= {9{1'b0}};
-		else if (user_sel_i && user_wr_i) read_addr <= user_addr_i[8:0];
+		if (!write_done_user_clk[1]) read_addr <= {10{1'b0}};
+		else if (user_sel_i && user_wr_i) read_addr <= user_addr_i[9:0];
 		else if (write_done_user_clk[1] && user_sel_i && user_rd_i)
 			read_addr <= read_addr + 1;
 	end	
 	
-	BRAM_SDP_MACRO #(.BRAM_SIZE("36Kb"),.DO_REG(0),
-						  .READ_WIDTH(72),.WRITE_WIDTH(72))
-						  u_R0_storage(.DI(R0_input),.WRADDR(write_addr),.WE(byte_write_enable),.WREN(write_enable),.WRCLK(sysclk_i),
-											.DO(R0_output),.RDADDR(read_addr),.RDEN(read_enable),.RDCLK(user_clk_i));
-	BRAM_SDP_MACRO #(.BRAM_SIZE("36Kb"),.DO_REG(0),
-						  .READ_WIDTH(72),.WRITE_WIDTH(72))
-						  u_R1_storage(.DI(R1_input),.WRADDR(write_addr),.WE(byte_write_enable),.WREN(write_enable),.WRCLK(sysclk_i),
-											.DO(R1_output),.RDADDR(read_addr),.RDEN(read_enable),.RDCLK(user_clk_i));
-	assign user_block0_data = R0_output[31:0];
-	assign user_block1_data = {R1_output[47:32],R0_output[47:32]};
-	assign user_block2_data = R1_output[31:0];
+	BRAM_SDP_MACRO #(.BRAM_SIZE("18Kb"),.DO_REG(0),
+						  .READ_WIDTH(36),.WRITE_WIDTH(36))
+						  u_A0_storage(.DI({{12{1'b0}},A[23:0]}),.WRADDR(write_addr),.WE(byte_write_enable),.WREN(write_enable),.WRCLK(sysclk_i),
+											.DO(A_output[0]),.RDADDR(read_addr[9:1]),.RDEN(read_enable),.RDCLK(user_clk_i));
+	BRAM_SDP_MACRO #(.BRAM_SIZE("18Kb"),.DO_REG(0),
+						  .READ_WIDTH(36),.WRITE_WIDTH(36))
+						  u_A1_storage(.DI({{12{1'b0}},A[47:24]}),.WRADDR(write_addr),.WE(byte_write_enable),.WREN(write_enable),.WRCLK(sysclk_i),
+											.DO(A_output[1]),.RDADDR(read_addr[9:1]),.RDEN(read_enable),.RDCLK(user_clk_i));
+	
+	BRAM_SDP_MACRO #(.BRAM_SIZE("18Kb"),.DO_REG(0),
+						  .READ_WIDTH(36),.WRITE_WIDTH(36))
+						  u_B0_storage(.DI({{12{1'b0}},B[23:0]}),.WRADDR(write_addr),.WE(byte_write_enable),.WREN(write_enable),.WRCLK(sysclk_i),
+											.DO(B_output[0]),.RDADDR(read_addr[9:1]),.RDEN(read_enable),.RDCLK(user_clk_i));
+	BRAM_SDP_MACRO #(.BRAM_SIZE("18Kb"),.DO_REG(0),
+						  .READ_WIDTH(36),.WRITE_WIDTH(36))
+						  u_B1_storage(.DI({{12{1'b0}},B[47:24]}),.WRADDR(write_addr),.WE(byte_write_enable),.WREN(write_enable),.WRCLK(sysclk_i),
+											.DO(B_output[1]),.RDADDR(read_addr[9:1]),.RDEN(read_enable),.RDCLK(user_clk_i));
+	
+	BRAM_SDP_MACRO #(.BRAM_SIZE("18Kb"),.DO_REG(0),
+						  .READ_WIDTH(36),.WRITE_WIDTH(36))
+						  u_C0_storage(.DI({{12{1'b0}},C[23:0]}),.WRADDR(write_addr),.WE(byte_write_enable),.WREN(write_enable),.WRCLK(sysclk_i),
+											.DO(C_output[0]),.RDADDR(read_addr[9:1]),.RDEN(read_enable),.RDCLK(user_clk_i));
+	BRAM_SDP_MACRO #(.BRAM_SIZE("18Kb"),.DO_REG(0),
+						  .READ_WIDTH(36),.WRITE_WIDTH(36))
+						  u_C1_storage(.DI({{12{1'b0}},C[47:24]}),.WRADDR(write_addr),.WE(byte_write_enable),.WREN(write_enable),.WRCLK(sysclk_i),
+											.DO(C_output[1]),.RDADDR(read_addr[9:1]),.RDEN(read_enable),.RDCLK(user_clk_i));
+	
+	BRAM_SDP_MACRO #(.BRAM_SIZE("18Kb"),.DO_REG(0),
+						  .READ_WIDTH(36),.WRITE_WIDTH(36))
+						  u_D0_storage(.DI({{12{1'b0}},D[23:0]}),.WRADDR(write_addr),.WE(byte_write_enable),.WREN(write_enable),.WRCLK(sysclk_i),
+											.DO(D_output[0]),.RDADDR(read_addr[9:1]),.RDEN(read_enable),.RDCLK(user_clk_i));
+	BRAM_SDP_MACRO #(.BRAM_SIZE("18Kb"),.DO_REG(0),
+						  .READ_WIDTH(36),.WRITE_WIDTH(36))
+						  u_D1_storage(.DI({{12{1'b0}},D[47:24]}),.WRADDR(write_addr),.WE(byte_write_enable),.WREN(write_enable),.WRCLK(sysclk_i),
+											.DO(D_output[1]),.RDADDR(read_addr[9:1]),.RDEN(read_enable),.RDCLK(user_clk_i));
+	
+	BRAM_SDP_MACRO #(.BRAM_SIZE("18Kb"),.DO_REG(0),
+						  .READ_WIDTH(36),.WRITE_WIDTH(36))
+						  u_E0_storage(.DI({{12{1'b0}},E[23:0]}),.WRADDR(write_addr),.WE(byte_write_enable),.WREN(write_enable),.WRCLK(sysclk_i),
+											.DO(E_output[0]),.RDADDR(read_addr[9:1]),.RDEN(read_enable),.RDCLK(user_clk_i));
+	BRAM_SDP_MACRO #(.BRAM_SIZE("18Kb"),.DO_REG(0),
+						  .READ_WIDTH(36),.WRITE_WIDTH(36))
+						  u_E1_storage(.DI({{12{1'b0}},E[47:24]}),.WRADDR(write_addr),.WE(byte_write_enable),.WREN(write_enable),.WRCLK(sysclk_i),
+											.DO(E_output[1]),.RDADDR(read_addr[9:1]),.RDEN(read_enable),.RDCLK(user_clk_i));
+	
+	BRAM_SDP_MACRO #(.BRAM_SIZE("18Kb"),.DO_REG(0),
+						  .READ_WIDTH(36),.WRITE_WIDTH(36))
+						  u_F0_storage(.DI({{12{1'b0}},F[23:0]}),.WRADDR(write_addr),.WE(byte_write_enable),.WREN(write_enable),.WRCLK(sysclk_i),
+											.DO(F_output[0]),.RDADDR(read_addr[9:1]),.RDEN(read_enable),.RDCLK(user_clk_i));
+	BRAM_SDP_MACRO #(.BRAM_SIZE("18Kb"),.DO_REG(0),
+						  .READ_WIDTH(36),.WRITE_WIDTH(36))
+						  u_F1_storage(.DI({{12{1'b0}},F[47:24]}),.WRADDR(write_addr),.WE(byte_write_enable),.WREN(write_enable),.WRCLK(sysclk_i),
+											.DO(F_output[1]),.RDADDR(read_addr[9:1]),.RDEN(read_enable),.RDCLK(user_clk_i));
+	
+											
+	// Pull off the bottom 24 bits that contain 8 3-bit samples										
+	assign user_block_data[0] = A_output[read_addr[0]][24:0];
+	assign user_block_data[1] = B_output[read_addr[0]][24:0];
+	assign user_block_data[2] = C_output[read_addr[0]][24:0];
+	assign user_block_data[3] = D_output[read_addr[0]][24:0];
+	assign user_block_data[4] = E_output[read_addr[0]][24:0];
+	assign user_block_data[5] = F_output[read_addr[0]][24:0];
+	assign user_block_data[6] = C_output[read_addr[0]][24:0];
+	assign user_block_data[7] = D_output[read_addr[0]][24:0];
+
 	assign done_o = write_done_user_clk[1];
 	assign sync_latch_o = addr0_sync_user_clk[1];
 endmodule
